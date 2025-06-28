71947befb19deadad5959e132e7a7247dbc9120b
swethasj12 <swethasenthilnathan.j12@gmail.com>
2025-06-28 21:45:24 +0530

    Create Calci.java

---
+++ Week 2/TDD Using JUnit 5/Exercise 4-Arrange-Act-Assert(AAA)Pattern,Text Fixtures,Setup and Teardown Methods in JUnit/Calci.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class Calci {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Division by zero");
+        return a / b;
+    }
+}
