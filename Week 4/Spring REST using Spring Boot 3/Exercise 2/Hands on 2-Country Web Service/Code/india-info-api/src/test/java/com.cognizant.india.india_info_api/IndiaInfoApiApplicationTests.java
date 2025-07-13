cba7cbccf3273f8eac53c9de97807affe8d1be1d
swethasj12 <swethasenthilnathan.j12@gmail.com>
2025-07-14 00:55:54 +0530

    Create IndiaInfoApiApplicationTests.java

---
+++ Week 4/Spring REST using Spring Boot 3/Exercise 2/Hands on 2-Country Web Service/Code/india-info-api/src/test/java/com.cognizant.india.india_info_api/IndiaInfoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.india.india_info_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndiaInfoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
