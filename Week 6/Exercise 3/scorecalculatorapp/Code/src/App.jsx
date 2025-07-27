a09008e76507e64d35c3932d30e902a38891f19b
swethasj12 <swethasenthilnathan.j12@gmail.com>
2025-07-28 01:06:39 +0530

    Add files via upload

---
+++ Week 6/Exercise 3/scorecalculatorapp/Code/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+
+import React from 'react';
+import CalculateScore from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div>
+      <CalculateScore Name="SWETHA S" School="ABC SCHOOL" Total={450} />
+    </div>
+  );
+}
+
+export default App;
