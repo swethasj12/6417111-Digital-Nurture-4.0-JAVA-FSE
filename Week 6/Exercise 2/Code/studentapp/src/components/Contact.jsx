44573b7f66cae3c0bc4153dae30a68853913870f
swethasj12 <swethasenthilnathan.j12@gmail.com>
2025-07-28 00:46:41 +0530

    Add files via upload

---
+++ Week 6/Exercise 2/Code/studentapp/src/components/Contact.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Contact() {
+  return <h3>Welcome to the Contact Page of Student Management Portal</h3>;
+}
+
+export default Contact;
---
+++ Week 6/Exercise 2/Code/studentapp/src/components/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <h3>Welcome to the Home Page of Student Management Portal</h3>;
+}
+
+export default Home;
