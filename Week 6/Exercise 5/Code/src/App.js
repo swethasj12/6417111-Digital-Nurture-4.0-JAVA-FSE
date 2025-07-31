d396b4013cc1cacb9478dedaf3d65e614a17372e
swethasj12 <swethasenthilnathan.j12@gmail.com>
2025-07-31 22:34:30 +0530

    Add files via upload

---
+++ Week 6/Exercise 5/Code/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import CohortDetails from './CohortDetails';
+import CohortDetails from './components/CohortDetails';
+
+function App() {
+  return (
+    <div className="App">
+      <CohortDetails />
+    </div>
+  );
+}
+
+export default App;
