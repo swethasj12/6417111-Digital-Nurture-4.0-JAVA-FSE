40f8c30f42679b4e1fc2e887909d55a7290c059b
swethasj12 <swethasenthilnathan.j12@gmail.com>
2025-08-07 00:51:13 +0530

    Add files via upload

---
+++ Week 7/React/Exercise 13/bloggerapp/Code/src/data.js
@@ -0,0 +1,16 @@
+// src/data.js
+export const books = [
+  { id: 101, bname: 'Master React', price: 670 },
+  { id: 102, bname: 'Deep Dive into Angular 11', price: 800 },
+  { id: 103, bname: 'Mongo Essentials', price: 450 },
+];
+
+export const courses = [
+  { id: 1, cname: 'Angular', date: '4/5/2021' },
+  { id: 2, cname: 'React', date: '6/3/2021' },
+];
+
+export const blogs = [
+  { title: 'React Learning', author: 'Stephen Biz', content: 'Welcome to learning React!' },
+  { title: 'Installation', author: 'Schwetzdnier', content: 'You can install React from npm.' },
+];
